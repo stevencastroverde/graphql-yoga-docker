454cfb634267a080030af39c0176ae25ad1477d7
Steven Castroverde <stevencastroverde@steven.local>
2019-06-27 22:36:47 -0600

    docker compose works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({ path: 'variables.env'});
+require('dotenv').config();
 const createServer = require('./createServer');
 const db = require('./db');
 
